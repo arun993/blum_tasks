e8cd79becf77e90d32a23ec057be2533d2818413
0xGareeb <117015135+arun993@users.noreply.github.com>
2024-09-08 21:37:55 +0530

    Create tasks_blum.sh

---
+++ tasks_blum.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Update system and install required packages
+sudo apt update
+sudo apt install -y python3 python3-pip git
+
+# Clone the repository
+git clone https://github.com/arun993/blum_tasks.git
+
+# Navigate to the cloned directory
+cd blumtod
+
+# Install required Python packages
+python3 -m pip install -r requirements.txt
+
+# Prompt the user to enter query IDs and store them in data.txt
+echo -e "\e[34mEnter your query_IDs line by line (Press Ctrl+D when done):\e[0m"
+cat > data.txt
+
+# Run the Python bot script
+python3 bot.py
